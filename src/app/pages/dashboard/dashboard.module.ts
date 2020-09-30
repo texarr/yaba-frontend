bf2be664e5bc7ad17321ecd44651483a8342f0db
Daniel Pietrucha <d.pietrucha@gmi.pl>
2020-09-30 22:33:33 +0200

    Fix translations from components class;

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -3,17 +3,16 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { BudgetPlanComponent } from './budget-plan/budget-plan.component';
 import { BudgetProgressComponent } from './budget-progress/budget-progress.component';
-import { TranslocoModule } from '@ngneat/transloco';
 import { UiComponentsModule } from '../../ui-components/ui-components.module';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { PanelMenuModule } from 'primeng/panelmenu';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [DashboardComponent, BudgetPlanComponent, BudgetProgressComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule,
-    TranslocoModule,
     UiComponentsModule,
     PanelMenuModule
   ]
